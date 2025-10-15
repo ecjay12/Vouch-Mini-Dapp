5cee164bb21fa4d8a743df59341da5ecc912d8bb
@ecjay12 <crytojay@yahoo.com>
2025-10-14 23:16:24 -0700

    “Update UP provider and fix async errors”

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: { extend: { colors: { teal: { 100: '#e6f3f7', 500: '#0d9488', 600: '#0891b2', 800: '#115e59' } } } },
-=======
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
@@ -18,6 +12,5 @@ export default {
       }
     }
   },
->>>>>>> 07726ff (Initial commit for Ohana Vouch MiniApp)
   plugins: [],
 };
